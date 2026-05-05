f75d996e6fff48a2e0f93c05a1feb19fd7a08029
EbbiSK <ebbi@ebbi.sk>
2026-05-05 18:01:54 +0200

    fix css import

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-import { NotificationsProvider } from './context/NotificationsContext';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <NotificationsProvider>
-      <App />
-    </NotificationsProvider>
+    <App />
   </React.StrictMode>
 );
